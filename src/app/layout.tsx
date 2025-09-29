1d385a41a470eb33dcf81e9067708be835c764d7
M <mari.v.zinkevich@gmail.com>
2025-09-29 18:50:44 +0300

    feat: update fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,38 @@ export const metadata: Metadata = {
   description: "...",
 };
 
+const fontDihjauti = localFont({
+  src: "../../public/fonts/Dihjauti-Regular.woff",
+  variable: "--font-dihjauti",
+});
+
+const fontAileron = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Aileron-Light.woff",
+      weight: "300",
+    },
+    {
+      path: "../../public/fonts/Aileron-Light.woff",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/Aileron-Regular.woff",
+      weight: "400",
+    },
+    {
+      path: "../../public/fonts/Aileron-SemiBold.woff",
+      weight: "600",
+    },
+    {
+      path: "../../public/fonts/Aileron-Bold.woff",
+      weight: "700",
+    },
+  ],
+  variable: "--font-aileron",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
