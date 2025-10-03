733d75e5cff630950f77e42519cb25b48dc4c0d4
M <mari.v.zinkevich@gmail.com>
2025-10-03 14:41:25 +0300

    feat: add hero section

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,5 +1,12 @@
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
 export default function Home() {
-  return <div className={styles.page}>это главная страница</div>;
+  return (
+    <main className={styles.main}>
+      <section className={styles.hero}>
+        <div className={styles.heroLeft}></div>
+        <div className={styles.heroRight}></div>
+      </section>
+    </main>
+  );
 }
