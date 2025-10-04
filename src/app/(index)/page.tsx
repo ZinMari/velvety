8d8a569d5737d7809220597b0f67e969f8cfb46b
M <mari.v.zinkevich@gmail.com>
2025-10-04 16:00:30 +0300

    feat: add features section

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -7,6 +7,17 @@ export default function Home() {
         <div className={styles.heroLeft}></div>
         <div className={styles.heroRight}></div>
       </section>
+      <section className={styles.features}>
+        <div className={styles.featuresLeft}>
+          <h3 className={styles.featuresTitle}>
+            Inspired by&nbsp;traditional knowledge and nature
+          </h3>
+          <div className={styles.featuresImage}>
+            <img src="./features.png" alt="" />
+          </div>
+        </div>
+        <div className={styles.featuresRight}></div>
+      </section>
     </main>
   );
 }
