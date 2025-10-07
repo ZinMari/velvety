de4a3e286f524133f364fdb80742c456dd0b86b6
M <mari.v.zinkevich@gmail.com>
2025-10-07 17:39:13 +0300

    feat: add Icons component

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,6 +1,30 @@
 import Container from "../../../ui/Container";
 import Typography from "../../../ui/Typography";
+import { IconOrganic, IconSerum, IconSuitable } from "../../../ui/Icons";
+
 import styles from "./page.module.scss";
+import clsx from "clsx";
+
+const FEATURES_DATA = [
+  {
+    id: 1,
+    title: "100% Organic",
+    text: "We craft skincare using the most exquisite ingredients from the plant, earth and mineral realms.",
+    icon: IconOrganic,
+  },
+  {
+    id: 2,
+    title: "Fits your skin",
+    text: "I’ts all natural and processed based on traditional knowledge with modern technology.",
+    icon: IconSuitable,
+  },
+  {
+    id: 3,
+    title: "Easy to use",
+    text: "Packed with a unique design as well as usefull that can help you perform routine skin care.",
+    icon: IconSerum,
+  },
+];
 
 export default function Home() {
   return (
@@ -19,7 +43,21 @@ export default function Home() {
               <img src="./features.png" alt="" />
             </div>
           </div>
-          <div className={styles.featuresRight}></div>
+          <div className={styles.featuresRight}>
+            <ul className={clsx(styles.featuresList, styles.list)}>
+              {FEATURES_DATA.map((elem) => (
+                <li key={elem.id} className={styles.listItem}>
+                  <div className={styles.listImg}>
+                    <elem.icon />
+                  </div>
+                  <div className={styles.listInfo}>
+                    <Typography as="h4">{elem.title}</Typography>
+                    <Typography as="p">{elem.text}</Typography>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
         </Container>
       </section>
     </main>
