aa4ef7f44c18775d393f1ea1e072514dff48021b
M <mari.v.zinkevich@gmail.com>
2025-10-09 12:42:28 +0300

    feat: create brands section

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -26,6 +26,39 @@ const FEATURES_DATA = [
   },
 ];
 
+const BRANDS_DATA = [
+  {
+    id: 1,
+    name: "vogue",
+    src: "./brands/vogue.svg",
+    link: "",
+  },
+  {
+    id: 2,
+    name: "forbes",
+    src: "./brands/forbes.svg",
+    link: "",
+  },
+  {
+    id: 3,
+    name: "thought catalog",
+    src: "./brands/th_catalog.svg",
+    link: "",
+  },
+  {
+    id: 4,
+    name: "women's health",
+    src: "./brands/wh.svg",
+    link: "",
+  },
+  {
+    id: 5,
+    name: "wwd",
+    src: "./brands/wwd.svg",
+    link: "",
+  },
+];
+
 export default function Home() {
   return (
     <main className={styles.main}>
@@ -94,6 +127,26 @@ export default function Home() {
               anti-inflammatory essential oils, anti-age soaps and creams, etc.
             </Typography>
           </div>
+        </Container>
+      </section>
+      <section className={styles.brands}>
+        <Container>
+          <Typography className={styles.brandsTitle} as="h2">
+            As&nbsp;seen&nbsp;in
+          </Typography>
+          <ul className={styles.brandsList}>
+            {BRANDS_DATA.map((elem) => (
+              <li key={elem.id}>
+                <a href={elem.link}>
+                  <img
+                    className={styles.brandsImg}
+                    src={elem.src}
+                    alt={elem.name}
+                  />
+                </a>
+              </li>
+            ))}
+          </ul>
         </Container>
       </section>
     </main>
