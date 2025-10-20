fa657154b4da14cf020ec2b4152f0ff19213affd
M <mari.v.zinkevich@gmail.com>
2025-10-20 09:28:58 +0300

    feat: update PRODUCTS_DATA

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -67,13 +67,14 @@ const BRANDS_DATA = [
   },
 ];
 
-const BEST_PRODUCTS_DATA = [
+const PRODUCTS_DATA = [
   {
     id: 1,
     name: "chicori",
     src: "./products/chicori.png",
     price: 20,
     rating: 4.0,
+    isBestProduct: true,
   },
   {
     id: 2,
@@ -81,6 +82,7 @@ const BEST_PRODUCTS_DATA = [
     src: "./products/notorious.png",
     price: 23,
     rating: 5.0,
+    isBestProduct: true,
   },
   {
     id: 3,
@@ -88,6 +90,31 @@ const BEST_PRODUCTS_DATA = [
     src: "./products/holocena.png",
     price: 20,
     rating: 5.0,
+    isBestProduct: true,
+  },
+  {
+    id: 4,
+    name: "classwing",
+    src: "./products/classwing_mini.png",
+    price: 20,
+    rating: 5.0,
+    isBestProduct: false,
+  },
+  {
+    id: 5,
+    name: "inamorata",
+    src: "./products/inamorata_mini.png",
+    price: 12,
+    rating: 4.5,
+    isBestProduct: false,
+  },
+  {
+    id: 6,
+    name: "lightcool",
+    src: "./products/lightcool_mini.png",
+    price: 22.5,
+    rating: 5.0,
+    isBestProduct: false,
   },
 ];
 
@@ -157,21 +184,23 @@ export default function Home() {
         <div className={styles.bestProductsProducts}>
           <div className={styles.bestProductsContainer}>
             <ul className={styles.bestProductsList}>
-              {BEST_PRODUCTS_DATA.map((product, index) => {
-                const backgroundType = index % 2 ? "ellipse" : "square";
+              {PRODUCTS_DATA.filter((e) => e.isBestProduct).map(
+                (product, index) => {
+                  const backgroundType = index % 2 ? "ellipse" : "square";
 
-                return (
-                  <li className={styles.bestProductsItem} key={product.id}>
-                    <ProductCard
-                      backgroundType={backgroundType}
-                      productName={product.name}
-                      price={product.price}
-                      rating={product.rating}
-                      productImg={product.src}
-                    />
-                  </li>
-                );
-              })}
+                  return (
+                    <li className={styles.bestProductsItem} key={product.id}>
+                      <ProductCard
+                        backgroundType={backgroundType}
+                        productName={product.name}
+                        price={product.price}
+                        rating={product.rating}
+                        productImg={product.src}
+                      />
+                    </li>
+                  );
+                }
+              )}
             </ul>
           </div>
         </div>
