250d9fd562397ac234a718a6e888b720caed9803
M <mari.v.zinkevich@gmail.com>
2025-10-23 15:32:52 +0300

    feat: work on allProducts section

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -315,6 +315,7 @@ export default function Home() {
                 </li>
               ))}
             </ul>
+            <Button>Shop now</Button>
           </div>
         </Container>
       </section>
