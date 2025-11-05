3b94f98d6b2b48dd139e1c20d720d8f81e9af3de
ZinMari <mari.v.zinkevich@gmail.com>
2025-11-05 06:00:53 +0300

    feat: create and work on StarRating component

--- ui/Slider/index.tsx
+++ ui/Slider/index.tsx
@@ -1,9 +1,15 @@
+import StarRating from "../StarRating";
+
 interface SliderProps {
   className?: string;
 }
 
 const Slider: React.FC<SliderProps> = ({ className }) => {
-  return <div>Reviews</div>;
+  return (
+    <div>
+      <StarRating />
+    </div>
+  );
 };
 
 export default Slider;
