2de50ffc01d97a079dcb3c14e089cade8c7eb064
ZinMari <mari.v.zinkevich@gmail.com>
2025-11-07 09:52:14 +0300

    add button handlers and component props to Slider component

--- ui/Slider/index.tsx
+++ ui/Slider/index.tsx
@@ -1,15 +1,36 @@
 import StarRating from "../StarRating";
 
+import cl from "clsx";
+
+import styles from "./style.module.scss";
+import { useState } from "react";
+
 interface SliderProps {
+  items: React.ReactNode[];
   className?: string;
+  autoPlay: boolean;
+  autoPlayTime: number;
 }
 
-const Slider: React.FC<SliderProps> = ({ className }) => {
-  return (
-    <div>
-      <StarRating />
-    </div>
-  );
+const Slider: React.FC<SliderProps> = ({
+  className,
+  items,
+  autoPlay = true,
+  autoPlayTime = 1000,
+}) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+
+  const handlePreviousClick = () => {
+    setCurrentImageIndex(
+      currentImageIndex === 0 ? items.length - 1 : currentImageIndex - 1
+    );
+  };
+
+  const handleNextClick = () => {
+    // (currentIndex + 1) % totalImages гарантирует, что по достижению последнего изображения, оно автоматически вернется к первому.
+    setCurrentImageIndex((currentImageIndex + 1) % items.length);
+  };
+  return <div></div>;
 };
 
 export default Slider;
