a5a77745367d7bee5d7e9f4dac1cd394e3a9ab68
M <mari.v.zinkevich@gmail.com>
2025-11-13 15:35:01 +0300

    add styles to the right side of HeroSection

--- components/sections/HeroSection/index.tsx
+++ components/sections/HeroSection/index.tsx
@@ -3,16 +3,19 @@ import styles from "./style.module.scss";
 
 import Typography from "../../ui/Typography";
 import Button from "../../ui/Button";
+import Slider from "../../ui/Slider";
 
 const HeroSection: React.FC = () => {
   return (
     <section className={styles.hero}>
       <div className={styles.heroLeft}></div>
       <div className={styles.heroRight}>
-        <Typography className={styles.heroTitle} as="h1">
-          Let nature take care of&nbsp;your body and soul
-        </Typography>
-        <Button className={styles.heroButton}>Shop now</Button>
+        <div className={styles.heroContent}>
+          <Typography className={styles.heroTitle} as="h1">
+            Let nature take care of&nbsp;your body and soul
+          </Typography>
+          <Button className={styles.heroButton}>Shop now</Button>
+        </div>
       </div>
     </section>
   );
