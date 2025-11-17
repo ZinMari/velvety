bcd8683c1d7d47ad0da762afc427b462540f4699
M <mari.v.zinkevich@gmail.com>
2025-11-17 11:48:46 +0300

    move the menu from the header to a separate Menu component

--- components/ui/Header/index.tsx
+++ components/ui/Header/index.tsx
@@ -2,138 +2,11 @@ import clsx from "clsx";
 import Logo from "../Logo";
 
 import styles from "./style.module.scss";
+import Menu from "../Menu";
 
 interface HeaderProps {
   className?: string;
 }
-
-const MENU_DATA = [
-  [
-    {
-      id: 1,
-      title: "Pages",
-      url: "/",
-      children: [
-        {
-          id: 11,
-          title: "Home",
-          url: "/",
-          children: null,
-        },
-        {
-          id: 12,
-          title: "About",
-          url: "/",
-          children: null,
-        },
-        {
-          id: 13,
-          title: "Shop",
-          url: "/",
-          children: null,
-        },
-        {
-          id: 14,
-          title: "Product Details",
-          url: "/",
-          children: null,
-        },
-        {
-          id: 15,
-          title: "Checkout",
-          url: "/",
-          children: null,
-        },
-        {
-          id: 16,
-          title: "Checkout Paypal",
-          url: "/",
-          children: null,
-        },
-        {
-          id: 17,
-          title: "Blog",
-          url: "/",
-          children: null,
-        },
-        {
-          id: 18,
-          title: "Blog Detail",
-          url: "/",
-          children: null,
-        },
-        {
-          id: 19,
-          title: "Order Confirmation",
-          url: "/",
-          children: null,
-        },
-        {
-          id: 110,
-          title: "Licenses",
-          url: "/",
-          children: null,
-        },
-        {
-          id: 111,
-          title: "Changelog",
-          url: "/",
-          children: null,
-        },
-        {
-          id: 112,
-          title: "Contact Us",
-          url: "/",
-          children: null,
-        },
-        {
-          id: 113,
-          title: "404",
-          url: "/",
-          children: null,
-        },
-        {
-          id: 114,
-          title: "401",
-          url: "/",
-          children: null,
-        },
-        {
-          id: 115,
-          title: "Styleguide",
-          url: "/",
-          children: null,
-        },
-      ],
-    },
-    {
-      id: 2,
-      title: "Shop",
-      url: "/",
-      children: null,
-    },
-    {
-      id: 3,
-      title: "About",
-      url: "/",
-      children: null,
-    },
-  ],
-  [
-    {
-      id: 4,
-      title: "Login",
-      url: "/",
-      children: null,
-    },
-    {
-      id: 5,
-      title: "Cart(0)",
-      url: "/",
-      children: null,
-    },
-  ],
-];
 
 const Header: React.FC<HeaderProps> = ({ className }) => {
   return (
@@ -141,17 +14,9 @@ const Header: React.FC<HeaderProps> = ({ className }) => {
       <div className={styles.headerLogo}>
         <Logo />
       </div>
-      <nav className={styles.headerNav}>
-        {MENU_DATA.map((menu, index) => (
-          <ul className={styles.headerList} key={index}>
-            {menu.map((item) => (
-              <li key={item.id}>
-                <a href="#">{item.title}</a>
-              </li>
-            ))}
-          </ul>
-        ))}
-      </nav>
+      <div className={styles.headerMenu}>
+        <Menu />
+      </div>
     </header>
   );
 };
