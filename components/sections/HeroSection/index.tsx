c7fa263c4eb62a6b8a92fef27ea1aaaa8afe0d2b
M <mari.v.zinkevich@gmail.com>
2025-12-03 15:42:14 +0300

    add slider to heroSection

--- components/sections/HeroSection/index.tsx
+++ components/sections/HeroSection/index.tsx
@@ -5,10 +5,19 @@ import Typography from "../../ui/Typography";
 import Button from "../../ui/Button";
 import Slider from "../../ui/Slider";
 
+const SLIDER_ITEM = [
+  <img src="/products/chicori.png" />,
+  <img src="/products/holocena.png" />,
+];
+
 const HeroSection: React.FC = () => {
   return (
     <section className={styles.hero}>
-      <div className={styles.heroLeft}></div>
+      <div className={styles.heroLeft}>
+        <div className={styles.heroSlider}>
+          <Slider items={SLIDER_ITEM} />
+        </div>
+      </div>
       <div className={styles.heroRight}>
         <div className={styles.heroContent}>
           <Typography className={styles.heroTitle} as="h1">
