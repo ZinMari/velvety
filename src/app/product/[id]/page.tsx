1e46ce701d4259fd96d53114d39f51b90aba766c
ZinMari <mari.v.zinkevich@gmail.com>
2025-12-05 20:58:04 +0300

    update get data in product pages

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,12 +1,13 @@
+import a from "../../../../public/data/products/products.json";
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  const data = (await import("../../../../public/data/products/products.json"))
-    .default;
-  const currentProduct = data.filter((e) => e.id == Number(id));
+
+  const currentProduct = a.filter((e) => e.id == Number(id));
   return (
     <div>
       <img src={currentProduct[0].src} alt="" />
