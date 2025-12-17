81798d079d4841f88d29edf4eb240599edec5720
ZinMari <mari.v.zinkevich@gmail.com>
2025-12-17 16:29:14 +0300

    add buttons to product page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,15 +1,29 @@
+"use client";
+import { useEffect, useState } from "react";
+
 import Container from "../../../../components/ui/Container";
 import products from "../../../../public/data/products/products.json";
 
 import styles from "./page.module.scss";
+import Typography from "../../../../components/ui/Typography";
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-  const currentProduct = products.find((e) => e.id == Number(id));
+export default function Page({ params }: { params: Promise<{ id: string }> }) {
+  const [currentProduct, setCurrentProduct] = useState<null | TProduct>(null);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { id } = await params;
+      const product = products.filter((e) => e.id === Number(id));
+
+      if (product.length > 0) {
+        setCurrentProduct(product[0] as TProduct);
+      } else {
+        window.location.href = "/";
+      }
+    };
+
+    fetchProduct();
+  }, [params]);
 
   return (
     <main className={styles.product}>
@@ -18,10 +32,18 @@ export default async function Page({
           <img src={currentProduct?.src} alt="" />
         </div>
         <div className={styles.productInfo}>
-          <h1 className={styles.productName}>{currentProduct?.name}</h1>
+          <Typography className={styles.productName} as="h1">
+            {currentProduct?.name}
+          </Typography>
           <p className={styles.productName}>{`${currentProduct?.price.toFixed(
             2
           )} $`}</p>
+          <div className={styles.productCount}>
+            <button className={styles.productBtn}>-</button>
+            <span className={styles.productNumber}>1</span>
+            <button className={styles.productBtn}>+</button>
+          </div>
+          <button className={styles.productBtn}>Add to Cart</button>
         </div>
       </Container>
     </main>
